28fd4f6c5cde03637298597af69b661adb8e23d0
Rishab Arora <ra.rishab@gmail.com>
2013-01-14 00:57:21 +0530

    Corrected settings.py

--- __init__.py
+++ __init__.py
@@ -2,3 +2,8 @@
 # All I want it to accomplish is to generate a static version
 # of ArthikTangi.com (literally translates to, 'Financial Strain')
 
+import time
+
+from pelican.readers import comicreader
+from pelican.writers import comicgenerator
+
--- settings.py
+++ settings.py
@@ -1,16 +1,21 @@
-AUTHOR = u'Rishab Arora'
-SITENAME = u'Arthik Tangi'
-SITEURL = 'http://www.arthiktangi.com'
-TIMEZONE = 'Asia/Kolkata'
+settings = {
+    'AUTHOR': u'Rishab Arora',
+    'SITENAME' : u'Arthik Tangi',
+    'SITEURL' : 'http://www.arthiktangi.com',
+    
+    'TIMEZONE' : 'Asia/Kolkata',
+    
+    'GOOGLE_ANALYTICS' : "UA-xxxxx-1",
+    'DISQUS_ID' : 'xxxx',
+    
+    'COMIC_RST_DIR' : 'content',
+    'COMIC_IMG' : 'images',
+    
+    'EXTRA_MENU' : {('Archives','/archives.html'),},
+    
+    'COMIC_URL' : 'comic/%d/',
+    'COMIC_SAVEAS' : 'comic/%d/index.html',
+}
 
-GOOGLE_ANALYTICS = "UA-xxxxx-1"
-
-COMIC_RST_DIR = 'content'
-COMIC_IMG = 'images'
-
-EXTRA_MENU = {('Archives','/archives.html'),}
-
-COMIC_URL = 'comic/%d/'
-COMIC_SAVEAS = 'comic/%d/index.html'
-
-DISQUS_ID = 'xxxx'
+def get_settings()
+    return settings
