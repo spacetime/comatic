069c94e1212ca8fb3c91563bd7a8e1f419a21605
Rishab Arora <ra.rishab@gmail.com>
2013-01-14 01:56:05 +0530

    Added theme files

--- __init__.py
+++ __init__.py
@@ -2,8 +2,32 @@
 # All I want it to accomplish is to generate a static version
 # of ArthikTangi.com (literally translates to, 'Financial Strain')
 
+__version__ = "0.0.1"
+
 import time
 
 from pelican.readers import comicreader
 from pelican.writers import comicgenerator
+
+from pelican.settings import get_settings
+
+class Comatic(object):
+    def __init__(self, settings):
+        """
+        Do some initialization stuff here
+        """
+        self.output_path = settings['OUTPUT_PATH']
+        self.
+        
+    def init_path(self):
+        if not any(p in sys.path for p in ['', '.']):
+            sys.path.insert(0, '')
+
+    def run(self):
+        """
+        copy static files.
+        read comic rst.
+        write comics.
+        rejoice.
+        """
 
--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ settings = {
     'AUTHOR': u'Rishab Arora',
     'SITENAME' : u'Arthik Tangi',
     'SITEURL' : 'http://www.arthiktangi.com',
+    'TITLE' : u'Arthik Tangi: The webcomic that should\'ve died'
     
     'TIMEZONE' : 'Asia/Kolkata',
     
@@ -9,13 +10,16 @@ settings = {
     'DISQUS_ID' : 'xxxx',
     
     'COMIC_RST_DIR' : 'content',
-    'COMIC_IMG' : 'images',
+    'STATIC' : {'images',},
+    'OUTPUT_PATH' = 'output',
     
     'EXTRA_MENU' : {('Archives','/archives.html'),},
-    
-    'COMIC_URL' : 'comic/%d/',
-    'COMIC_SAVEAS' : 'comic/%d/index.html',
+
+    'THEME': 'arthiktangi',
+    'FOOTER': """
+ This work is licensed under a Creative Commons Attribution-NonCommercial 2.5 License. 
+"""
 }
 
-def get_settings()
+def get_settings():
     return settings
