3732e3bdb86c89774870cdd794cfd7ff98e5931e
Rishab Arora <ra.rishab@gmail.com>
2013-01-14 00:19:29 +0530

    Added license, and settings

---
+++ settings.py
@@ -0,0 +1,15 @@
+AUTHOR = u'Rishab Arora'
+SITENAME = u'Arthik Tangi'
+SITEURL = 'http://www.arthiktangi.com'
+TIMEZONE = 'Asia/Kolkata'
+
+GOOGLE_ANALYTICS = "UA-27046006-1"
+
+COMIC_RST_DIR = 'arbitfolder'
+COMIC_IMG = 'images'
+
+EXTRA_MENU = {('Archives','/archives.html'),}
+
+COMIC_URL = 'comic/%d/'
+COMIC_SAVEAS = 'comic/%d/index.html'
+
